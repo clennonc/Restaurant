fd754d88a951ef043bebe216faaaf40b7971224c
Clennonc <47015309+clennonc@users.noreply.github.com>
2020-12-20 20:31:39 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!--footer-->
+<footer class="footer" id="footer">
+ <div class="container"><br>
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="social-icon">
+        <a href="index.php"><i class="fa fa-facebook"></i></a>
+        <a href="index.php"><i class="fa fa-twitter"></i></a>
+        <a href="index.php"><i class="fa fa-pinterest"></i></a>
+        <a href="index.php"><i class="fa fa-rss"></i></a>
+      </div>
+      <div class="copyright">
+        <p class="white"> copyright &copy; <b>Cher's Palate</b></p>
+        <p><i class="fa fa-phone"></i> (876) 806-2748 &nbsp;<i class="fa fa-envelope-o"></i> clennon.cherry@gmail.com</p>
+      </div>
+    </div>
+  </div>
+ </div>
+</footer>
+<!--end of footer-->
+</body>
+<!---end of body-->
+</html>
